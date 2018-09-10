8db522c9795a871beb4dbe51b840b64ec711eea7
pushpa ghale <anthos77@hotmail.com>
2018-09-09 22:24:49 -0500

    latest updates

--- JSON_Test/Form1.Designer.cs
+++ JSON_Test/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Json Output";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 76);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(740, 257);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert To JSON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(608, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "JSON to Object";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- JSON_Test/Form1.cs
+++ JSON_Test/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace JSON_Test
 {
@@ -15,6 +9,49 @@ namespace JSON_Test
         public Form1()
         {
             InitializeComponent();
+            textBox1.ReadOnly = true;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.id = 22;
+            emp.fname = "pushpa";
+            emp.lname = "ghale";
+            emp.dept = "information technology";
+
+            string output = string.Empty;
+            output += @"Serializing the object below
+            Employee emp = new Employee();
+            emp.id = 22;
+            emp.fname = ""pushpa"";
+            emp.lname = ""ghale"";
+            emp.dept = ""information technology"";";
+
+            output += "\n Below is serialized object \n";
+            output += JsonConvert.SerializeObject(emp);
+            textBox1.Text = output;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string empJson = "{\"id\":22,\"fname\":\"pushpa\",\"lname\":\"ghale\",\"dept\":\"information technology\"}";
+
+            var empobj = JsonConvert.DeserializeObject<Employee>(empJson);
+
+            string output = string.Empty;
+            output += "Deserializing Json string below and casting it to employee object \n";
+            output += empJson + "\n";
+            output += "fname property from the new object: " + empobj.fname;
+            textBox1.Text = output;
+        }
+    }
+
+    class Employee
+    {
+        public int id { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string dept { get; set; }
     }
 }
